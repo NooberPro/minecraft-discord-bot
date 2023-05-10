530b2a0aa144cf3078b8e5c6a1662b94394730f4
27MrMan <128210486+27MrMan@users.noreply.github.com>
2023-05-10 18:42:49 +0000

    modified:   src/events/messageCreate/status-reply.js 	modified:   config.js 	modified:   src/events/messageCreate/status-reply.js

--- src/events/messageCreate/status-reply.js
+++ src/events/messageCreate/status-reply.js
@@ -4,8 +4,8 @@ module.exports = (message) => {
   if (config.status_reply.enabled === false) return;
   if (
     config.status_reply.triggerWords2.some((word) =>
-      message.content.includes(word))
-    message.reply('**The server's Version is 
+      message.content.includes(word)))
+    message.reply("**The server's version is **", config.Version);
   if (
     config.status_reply.triggerWords.some((word) =>
       message.content.includes(word)
