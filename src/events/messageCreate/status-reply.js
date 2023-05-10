a31cd6aacabbcbc64a39ea8028ed5c9f73a76859
27MrMan <128210486+27MrMan@users.noreply.github.com>
2023-05-10 14:23:22 -0400

    Update status-reply.js

--- src/events/messageCreate/status-reply.js
+++ src/events/messageCreate/status-reply.js
@@ -3,6 +3,10 @@ const { data } = require('../../index.js');
 module.exports = (message) => {
   if (config.status_reply.enabled === false) return;
   if (
+    config.status_reply.triggerWords2.some((word) =>
+      message.content.includes(word))
+    message.reply('**The server's IP is 
+  if (
     config.status_reply.triggerWords.some((word) =>
       message.content.includes(word)
     )
