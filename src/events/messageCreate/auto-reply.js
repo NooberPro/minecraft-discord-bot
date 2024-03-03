f4f9fb1f6ff15d505c8b1d9e7635f3911a711658
NooberPro <109412782+NooberPro@users.noreply.github.com>
2024-03-03 16:22:24 +0530

    1.7.2
    ### FIXED
    - Auto Reply not working

--- src/events/messageCreate/auto-reply.js
+++ src/events/messageCreate/auto-reply.js
@@ -9,19 +9,23 @@ const autoReplyReplyText = json5.parse(fileContents)
 module.exports = async (msg) => {
   try {
     if (!autoReply.enabled || msg.author.bot || msg.content.startsWith(commands.prefixCommands.prefix)) return
-    const words = msg.content.split(' ')
+    const { content } = msg
     const { ip, site, status, version } = autoReply
+    const isIp = new RegExp(`\\b(${ip.triggerWords.join('|')})\\b`)
+    const isSite = new RegExp(`\\b(${site.triggerWords.join('|')})\\b`)
+    const isStatus = new RegExp(`\\b(${status.triggerWords.join('|')})\\b`)
+    const isVersion = new RegExp(`\\b(${version.triggerWords.join('|')})\\b`)
 
-    if (words.some((word) => ip.triggerWords.includes(word)) && autoReply.ip.enabled) {
+    if (isIp.test(content) && autoReply.ip.enabled) {
       msg.reply(autoReplyReplyText.ip.replyText.replace(/{ip}/g, mcserver.ip).replace(/{port}/g, mcserver.port))
     }
-    if (words.some((word) => site.triggerWords.includes(word)) && autoReply.site.enabled && mcserver.site) {
+    if (isSite.test(content) && autoReply.site.enabled && mcserver.site) {
       msg.reply(autoReplyReplyText.site.replyText.replace(/{site}/g, mcserver.site))
     }
-    if (words.some((word) => version.triggerWords.includes(word)) && autoReply.version.enabled) {
+    if (isVersion.test(content) && autoReply.version.enabled) {
       msg.reply(autoReplyReplyText.version.replyText.replace(/{version}/g, mcserver.version))
     }
-    if (words.some((word) => status.triggerWords.includes(word)) && autoReply.status.enabled) {
+    if (isStatus.test(content) && autoReply.status.enabled) {
       await msg.channel.sendTyping()
       const { getServerDataOnly } = require('../../index')
       const { data, isOnline } = await getServerDataOnly()
